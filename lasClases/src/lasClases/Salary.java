00ad5a0d62df3f870211edf56d35ca33e82a26ae
gusmol37 <86698059+gusmol37@users.noreply.github.com>
2022-06-10 12:37:06 -0500

    Add files via upload
    Se hace ampliación de la documentación del código y se sube este archivo que registra los cambios. Solo se modificó la documentación. El código no fue modificado y permanece igual que en la fecha original en que fue entregado.

---
+++ lasClases/src/lasClases/Salary.java
@@ -0,0 +1,125 @@
+package lasClases;
+
+/**
+ * Class that allows the handling of methods and objects
+ * 
+ * @author Gustavo Molina <gusmol37@gmail.com>
+ * @version 1.0.0 2022-05-31
+ */
+
+/**
+ * Create the Salary class
+ */
+public class Salary {
+
+	/**
+	 * Declaration attribute private nameWorker of type String
+	 */
+	private String nameWorker;
+
+	/**
+	 * Declaration private hour attribute of type int
+	 */
+	private int hour;
+
+	/**
+	 * Declaration of private value attribute of type float
+	 */
+	private float value;
+
+	/**
+	 * Declaration protected total attribute of type float
+	 */
+	protected float total;
+
+	/**
+	 * Constructors
+	 *
+	 * @param nameWorker: worker's name
+	 * @param hour:       hour value
+	 * @param value:      amount per hour
+	 * @param total:      total salary
+	 */
+	public Salary(String nameWorker, int hour, float value) {
+		this.nameWorker = nameWorker;
+		this.hour = hour;
+		this.value = value;
+
+	}
+
+	/**
+	 * Method that captures the worker's name.
+	 * 
+	 * @return returns the worker's name.
+	 */
+	public String getNameWorker() {
+		return nameWorker;
+	}
+
+	/**
+	 * Method that assigns initial value to the worker's name.
+	 * 
+	 * @return value to the worker's name
+	 */
+	public void setNameWorker(String nameWorker) {
+		this.nameWorker = nameWorker;
+	}
+
+	/**
+	 * Method that captures hour.
+	 * 
+	 * @return returns hour.
+	 */
+	public int getHour() {
+		return hour;
+	}
+
+	/**
+	 * Method that assigns initial value to the hour.
+	 * 
+	 * @return value to the worker's hour
+	 */
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
+	/**
+	 * Method that captures hour value.
+	 * 
+	 * @return returns hour value.
+	 */
+	public float getValue() {
+		return value;
+	}
+
+	/**
+	 * Method that assigns initial to hour value.
+	 * 
+	 * @return value to hour value
+	 */
+	public void setValue(float value) {
+		this.value = value;
+	}
+
+	/**
+	 * Method that captures total value.
+	 * 
+	 * @return returns total value.
+	 */
+	public float getTotal() {
+		return total;
+	}
+
+	/**
+	 * Method that assigns initial to total value.
+	 * 
+	 * @return value to total value
+	 */
+	public void setTotal(float total) {
+		this.total = total;
+	}
+
+	public float valorTotal(int hour, float value) {
+		return (hour * value);
+	}
+}
